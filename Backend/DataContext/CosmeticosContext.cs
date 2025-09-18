e6539bd4bc355c5194b48c622fd6217f0a834fb1
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-09-18 15:28:11 -0300

    Elimina importaciones y mejora autenticación y UI
    Se eliminaron importaciones innecesarias en `CosmeticosContext.cs`.
    Se añadió la referencia al paquete `FirebaseAuthentication.net` en `Desktop.csproj` para integrar la autenticación de Firebase.
    Además, se mejoró la lógica del temporizador en `SplashView.cs` para que la ventana de inicio de sesión solo se muestre al completar la carga.

--- Backend/DataContext/CosmeticosContext.cs
+++ Backend/DataContext/CosmeticosContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Service.Enums;
 using Service.Models;
 
--- Desktop/Views/SplashView.cs
+++ Desktop/Views/SplashView.cs
@@ -20,10 +20,14 @@ namespace Desktop.Views
         private void TimerSplash_Tick(object sender, EventArgs e)
         {
             ProgressBar.Value+=2;
-            this.Hide();
-            var login = new LogInView();
-            login.ShowDialog();
-            this.Close();
+            if(ProgressBar.Value == 100)
+            {
+                TimerSplash.Stop();
+                this.Hide();
+                var logInView = new LogInView();
+                logInView.ShowDialog();
+                this.Close();
+            }
         }
     }
 }
