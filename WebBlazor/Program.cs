9dfa5f0eaa440013b171724b49badbfafc744eb6
ASUS X515M <ASUS X515M@XIMENA>
2025-09-24 21:42:52 -0300

    Añadir autenticación de usuarios con Firebase
    Se ha implementado un sistema de autenticación que incluye:
    - Nuevo enlace de navegación para "Iniciar Sesión" en `NavMenu.razor`.
    - Integración de SweetAlert2 para mostrar alertas en la aplicación.
    - Referencia al paquete `CurrieTechnologies.Razor.SweetAlert2` en el proyecto.
    - Inclusión de scripts de Firebase en `index.html` para la autenticación.
    - Creación de los componentes `LogIn.razor` y `SignIn.razor` para iniciar sesión y registrarse, respectivamente.
    - Implementación del servicio `FirebaseAuthService.cs` para manejar la autenticación de usuarios.

--- WebBlazor/Program.cs
+++ WebBlazor/Program.cs
@@ -1,14 +1,18 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Service.Interfaces;
 using Service.Services;
 using WebBlazor;
+using WebBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSweetAlert2();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericServices<>));
+builder.Services.AddScoped<FirebaseAuthService>();
 
 await builder.Build().RunAsync();
